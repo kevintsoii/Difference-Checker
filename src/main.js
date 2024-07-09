78ad36e1a5805bc366b92d10908a5e4111e790ac
kevintsoii <kevin.tsoi44@gmail.com>
2024-07-09 00:08:13 -0700

    initialize electron + react app

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from "electron";
+import { join } from "path";
+import isDev from "electron-is-dev";
+
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  const startURL = isDev
+    ? "http://localhost:3000"
+    : `file://${join(__dirname, "../build/index.html")}`;
+
+  window.loadURL(startURL);
+}
+
+app.on("ready", () => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
